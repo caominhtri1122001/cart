d8d9500c5d941ad6744f3e7b583a035c23fccfd3
Cao Minh Tri <caominhtri112201@gmail.com>
2022-08-08 15:13:33 +0700

    Updates

---
+++ precache-manifest.04ff4b4516ee7060cd241cedaa261a24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e2e038940df6ee38dc607636e552e7",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "ecbbb1ec78cdf4cf8d2d",
+    "url": "/cart/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/cart/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecbbb1ec78cdf4cf8d2d",
+    "url": "/cart/static/js/main.344a6f52.chunk.js"
+  },
+  {
+    "revision": "6f930286e52d694f2280",
+    "url": "/cart/static/js/runtime-main.526553cb.js"
+  }
+]);
